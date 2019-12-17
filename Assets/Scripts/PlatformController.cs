09e96be5983fc04bb5a62247ef54ee87126d4a2e
rickyjames1750 <rickyjames1750@gmail.com>
2019-12-17 04:13:08 -0800

    refactor: devloped PlatformController script on player object

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    // movement target 
+    public Vector3 target;
+
+    // speed
+    public float speed = 1;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // calculate the distance from target
+        float distance = Vector3.Distance(transform.position, target);
+
+        // have we arrived?
+        if(distance > 0) {
+
+            // calculate how much we need to move (step) d = v * t
+            float step = speed * Time.deltaTime;
+
+            // move by that step
+            transform.position = Vector3.MoveTowards(transform.position, target, step);
+        }
+    }
+}
